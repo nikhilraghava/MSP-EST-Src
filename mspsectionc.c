d958dc3e0e82a28d608dd1f2ab66a4e1bc711137
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2018-08-22 16:33:14 +0800

    minor fixes

--- mspsectionc.c
+++ mspsectionc.c
@@ -1,3 +1,4 @@
+#include "ae.h"
 #include <dos.h>
 
 #define DIGITALSENSOR 0x41
@@ -7,14 +8,16 @@ void main(void) {
   unsigned char inpt;
   unsigned char blink = 0xFF;
   unsigned char ledStrip = 0x01;
-  
+
   while(1) {
     inpt = inport(DIGITALSENSOR) & 0x04
     if (inpt) {
-      outport(LEDS, blink ^ 0xFF);
+      blink ^= 0xFF;
+      outport(LEDS, blink);
       delay_ms(300);
     } else {
-      outport(LEDS, ledStrip <<= 1);
+      ledStrip <<= 1;
+      outport(LEDS, ledStrip);
       delay_ms(500);
     }
   }
